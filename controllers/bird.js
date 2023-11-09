1f15884da6efbfbc4eb6568e75b496bef89f8642
harshinikolan <142836121+harshinikolan@users.noreply.github.com>
2023-11-08 23:55:11 -0600

    last

--- controllers/bird.js
+++ controllers/bird.js
@@ -8,9 +8,9 @@ exports.bird_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: bird detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.bird_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: bird create POST');
-};
+// exports.bird_create_post = function(req, res) {
+// res.send('NOT IMPLEMENTED: bird create POST');
+// };
 // Handle Costume delete form on DELETE.
 exports.bird_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: bird delete DELETE ' + req.params.id);
@@ -19,3 +19,50 @@ res.send('NOT IMPLEMENTED: bird delete DELETE ' + req.params.id);
 exports.bird_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: bird update PUT' + req.params.id);
 };
+
+// List of all Costumes
+exports.bird_list = async function(req, res) {
+    try{
+    thebirds = await bird.find();
+    res.send(thebirds);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
+
+// VIEWS
+// Handle a show all view
+exports.bird_view_all_Page = async function(req, res) {
+    try{
+    thebirds = await bird.find();
+    res.render('bird', { title: 'bird Search Results', results: thebirds });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+    exports.bird_create_post = async function(req, res) {
+        console.log(req.body)
+        let document = new bird();
+        // We are looking for a body, since POST does not have query parameters.
+        // Even though bodies can be in many different formats, we will be picky
+        // and require that it be a json object
+        // {"costume_type":"goat", "cost":12, "size":"large"}
+        document.birdName = req.body.birdName;
+        document.birdPrice = req.body.birdPrice;
+        document.birdColor = req.body.birdColor;
+        try{
+        let result = await document.save();
+        res.send(result);
+        }
+        catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+        }
+        };
+        
--- routes/bird.js
+++ routes/bird.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const bird_controlers= require('../controllers/bird');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('bird', { title: 'Search results bird' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('bird', { title: 'Search results bird' });
+// });
 
+router.get('/', bird_controlers.bird_view_all_Page );
 module.exports = router;
